101da6007d2188667cf6fa3965695fa9cc307a87
Florian Sandel <fsandel@1-A-25.42heilbronn.de>
2022-10-28 17:38:18 +0200

    first draft

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsandel <fsandel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/28 17:32:49 by fsandel           #+#    #+#             */
+/*   Updated: 2022/10/28 17:37:41 by fsandel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdio.h>
+
+char	*get_next_line(int fd);
+
+#endif
