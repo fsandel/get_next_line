0cf226192eb32b30de041df3411e57d429ad60c8
Florian Sandel <fsandel@1-A-25.42heilbronn.de>
2022-10-31 10:12:53 +0100

    Makefile patch

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: fsandel <fsandel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 17:32:46 by fsandel           #+#    #+#             */
-/*   Updated: 2022/10/31 10:08:59 by fsandel          ###   ########.fr       */
+/*   Updated: 2022/10/31 10:12:22 by fsandel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,19 @@ char	*get_next_line(int fd)
 	int			byte_read;
 	static char	*leftovers = NULL;
 
-	BUFFER_SIZE = 100;
+	BUFFER_SIZE = 1;
 	buffer = ft_calloc(BUFFER_SIZE + 1, 1);
 	output = leftovers;
 	byte_read = 1;
 	while (byte_read > 0)
 	{
 		byte_read = read(fd, buffer, BUFFER_SIZE);
-		if (ft_strchr(buffer, '\n'))
-		{
-			leftovers = ft_strdup(ft_strchr(buffer, '\n') + 1);
-			buffer = ft_cut_newline(buffer);
-			output = ft_strjoin(output, buffer);
-			return (output);
-		}
 		output = ft_strjoin(output, buffer);
+		if (ft_strchr(output, '\n'))
+		{
+			leftovers = ft_strdup(ft_strchr(output, '\n') + 1);
+			return (ft_cut_newline(output));
+		}
 	}
 	return (0);
 }
